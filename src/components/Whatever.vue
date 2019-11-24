f0a94b20cf382369fbf061f5dc315b266efaa68b
Zhao <zhaoshangming1997@gmail.com>
2019-11-24 17:48:43 -0500

    first commit

---
+++ src/components/Whatever.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Whatever",
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = "http://localhost:5000/whatever";
+      axios
+        .get(path)
+        .then(res => {
+          this.msg = res.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.getMessage();
+  }
+};
+</script>
