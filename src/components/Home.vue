458f7f113a8689c23e5430ed2242df8586297c59
Zhao <zhaoshangming1997@gmail.com>
2019-11-28 20:54:01 -0500

    finished home skeleton. added some sample code for backend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,49 +1,99 @@
 <template>
   <div>
-    <v-row no-gutters>
   <v-app-bar
       color="amber"
     >
-    <span class="title mx-3">Yelp</span>
-    <v-switch
-      class="mt-6"
-      v-model="forChinese"
-      color="red"
-    ></v-switch>
-    <span v-bind:class="[{ 'grey--text': !forChinese }, 'title ml-0 mr-3']">For Chinese</span>
-    <v-text-field
-      solo-inverted
-      flat
-      hide-details
-      label="Search"
-    />
-    <v-btn text icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
-  </v-app-bar>
+    <v-row align="center">
+      <span class="title ma-3">Yelp</span>
+      <v-switch
+        hide-details
+        v-model="forChinese"
+        color="red"
+      ></v-switch>
+      <span v-bind:class="[{ 'grey--text': !forChinese }, 'title ml-0 mr-3']">For Chinese</span>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search">
+      </v-text-field>
+      <v-btn text icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
     </v-row>
-  <!--<v-row no-gutters>-->
-    <!--<v-col class="text-center" cols="3">-->
-      <!--<div class="my-1">-->
-        <!--<v-btn block>Burgers</v-btn>-->
-      <!--</div>-->
-    <!--</v-col>-->
-    <!--<v-col class="text-center" cols="3">-->
-      <!--<div class="my-1">-->
-        <!--<v-btn block>Italian</v-btn>-->
-      <!--</div>-->
-    <!--</v-col>-->
-    <!--<v-col class="text-center" cols="3">-->
-      <!--<div class="my-1">-->
-        <!--<v-btn block>Chinese</v-btn>-->
-      <!--</div>-->
-    <!--</v-col>-->
-    <!--<v-col class="text-center" cols="3">-->
-      <!--<div class="my-1">-->
-        <!--<v-btn block>Mexican</v-btn>-->
-      <!--</div>-->
-    <!--</v-col>-->
-  <!--</v-row>-->
+
+  </v-app-bar>
+    <v-card cols="12" class="mt-1 mx-auto">
+      <v-card-text class="pb-0">
+        <div class="d-flex justify-space-between mr-3">
+          <div>
+            <div class="title" style="color: grey">
+              Category
+            </div>
+            <v-divider class="mt-3"></v-divider>
+            <v-row>
+            <v-checkbox
+              v-for="category in categories"
+              :key="category"
+              :label="category"
+              color="red"
+              :value="category"
+              class="mt-2 ml-5"
+              hide-details
+              dense
+            ></v-checkbox>
+            </v-row>
+          </div>
+
+          <div>
+            <div class="title" style="color: grey">
+              Price
+            </div>
+            <v-divider class="mt-3"></v-divider>
+            <v-row>
+              <v-checkbox
+                v-for="price in prices"
+                :key="price"
+                :label="`$`.repeat(price)"
+                color="red"
+                :value="price"
+                class="mt-2 ml-5"
+                hide-details
+                dense
+              ></v-checkbox>
+            </v-row>
+          </div>
+
+          <div>
+            <div class="title" style="color: grey">
+              Rating
+            </div>
+            <v-divider class="mt-3"></v-divider>
+            <v-row>
+              <v-checkbox
+                v-for="rating in ratings"
+                :key="rating"
+                :label="rating"
+                color="red"
+                :value="rating"
+                class="mt-2 ml-5"
+                hide-details
+                dense
+              ></v-checkbox>
+            </v-row>
+          </div>
+        </div>
+      </v-card-text>
+
+      <v-card-actions class="justify-end">
+        <v-btn
+          text
+          color="red"
+        >
+          filter
+        </v-btn>
+      </v-card-actions>
+    </v-card>
 
 
     <v-row no-gutters>
@@ -51,19 +101,19 @@
         v-for="(item, i) in items"
         :key="i"
         cols="12"
-        class="my-1"
+        class="mt-1"
       >
         <v-card>
           <div class="d-flex flex-no-wrap justify-space-between">
             <!--avatar-->
             <div class="mx-4 my-auto">
-            <v-avatar
-              style="border-radius: 3%;"
-              size="250"
-              tile
-            >
-              <v-img :src="item.img"></v-img>
-            </v-avatar>
+              <v-avatar
+                style="border-radius: 3%;"
+                size="250"
+                tile
+              >
+                <v-img :src="item.img"></v-img>
+              </v-avatar>
             </div>
 
             <!--basic information-->
@@ -79,7 +129,7 @@
                 >
                   <v-rating
                     :value="item.rating"
-                    color="amber"
+                    color="red"
                     dense
                     half-increments
                     readonly
@@ -107,94 +157,78 @@
             <!--popular review-->
             <div>
               <v-card-text class="pl-0">
-                {{ item.popularReview }}
+                {{ item.popularReviews[0] }}
               </v-card-text>
             </div>
           </div>
         </v-card>
       </v-col>
     </v-row>
+
+    <!--scroll to top-->
     <v-btn
-            v-scroll="onScroll"
-            v-show="fab"
-            fab
-            dark
-            fixed
-            bottom
-            right
-            color="red"
-            @click="toTop"
-          >
-            <v-icon>mdi-chevron-up</v-icon>
-          </v-btn>
+      v-scroll="onScroll"
+      v-show="fab"
+      fab
+      dark
+      fixed
+      bottom
+      right
+      color="red"
+      @click="toTop"
+    >
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
+
+    <!--pagination-->
     <div class="text-center">
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="6">
-          <v-container class="max-width">
-            <v-pagination
-              v-model="page"
-              class="my-4"
-              :length="pageLength"
-            ></v-pagination>
-          </v-container>
-        </v-col>
-      </v-row>
-    </v-container>
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="6">
+            <v-container class="max-width">
+              <v-pagination
+                v-model="page"
+                class="my-4"
+                :length="pageLength"
+                color="red"
+              ></v-pagination>
+            </v-container>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
   </div>
-
-  </div>
-
 </template>
 
 <script>
+  import axios from "axios";
   export default {
     data: () => ({
-      totalItems: Array(100).fill({
-          img: 'https://s3-media0.fl.yelpcdn.com/bphoto/hd7Ith2Gm3QnrQqOzI_JvA/ls.jpg',
-          rank: 1,
-          rating: 4.5,
-          numReviews: 3019,
-          cost: 2,
-          name: 'Poor Calvin\'s',
-          type: 'Asian Fusion, Southern, Comfort Food',
-          phone: '(404) 254-4051',
-          address: '510 Piedmont Ave NE',
-          neighborhood: 'Downtown',
-          popularReview: '“Amazing place! Took an off from work just to experience this place! It was so worth it! Amazing food. Tried \'The Beast\' sandwich and the \'shawrma platter\'.…”'
-        }).concat(Array(100).fill({
-          img: 'https://s3-media0.fl.yelpcdn.com/bphoto/_x61pM4WZiji4d2jq1GnYw/ls.jpg',
-          rank: 2,
-          rating: 5.0,
-          numReviews: 1433,
-          cost: 2,
-          name: 'Aviva by Kameel',
-          type: 'Mediterranean, Juice Bars & Smoothies, Middle Eastern',
-          phone: '(404) 698-3600',
-          address: '225 Peachtree St NE',
-          neighborhood: 'Downtown',
-          popularReview: '“Hubby and I were in town for the weekend from CA and wanted to try some yummy fried chicken so took to Yelp to search. Came across Gus\'s and was super happy…”'
-        })),
+      totalItems: [],
       page: 1,
-      pageLength: 20,
       fab: false,
       forChinese: true,
+      categories: ['Burgers', 'Italian', 'Chinese', 'Mexican'],
+      prices: [1, 2, 3, 4],
+      ratings: ['0-1', '1-2', '2-3', '3-4', '4-5'],
     }),
 
     computed: {
       items: function() {
         return this.totalItems.slice((this.page - 1) * 10, this.page * 10 + 1);
+      },
+      pageLength: function() {
+        return Math.ceil(this.totalItems.length / 10);
       }
     },
 
     watch: {
       page: function(value) {
-        console.log('page ' + value);
+        this.page = value;
         this.toTop();
       },
-
-      forChinese: function(value) {
-        console.log('for chinses' + value);
+      forChinese: function() {
+        this.getTotalItems();
       }
     },
 
@@ -205,13 +239,30 @@
         this.fab = top > 20;
       },
       toTop () {
-        console.log('toTop');
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth'});
+      },
+      getTotalItems() {
+        const path = "http://localhost:5000/totalItems";
+        const params = { forChinese: this.forChinese };
+        console.log(params);
+        axios
+          .get(path, { params })
+          .then(res => {
+            this.totalItems = res.data;
+            console.log(this.totalItems);
+          })
+          .catch(error => {
+            console.error(error);
+          });
       }
     },
 
     mounted: function() {
       window.scrollTo({ top: 0, left: 0});
+    },
+
+    created: function() {
+      this.getTotalItems();
     }
   }
 </script>
